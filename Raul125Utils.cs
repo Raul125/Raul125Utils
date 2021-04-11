189468eaeb098a5e106b05dde3bd8f14969ee3e8
Raul125 <71040879+Raul125@users.noreply.github.com>
2021-04-11 22:01:25 +0200

    Version Push

--- Raul125Utils.cs
+++ Raul125Utils.cs
@@ -39,7 +39,7 @@ namespace Raul125Utils
         public override string Prefix { get; } = "Raul125Utils";
 
         /// <inheritdoc/>
-        public override Version Version { get; } = new Version(1, 0, 1);
+        public override Version Version { get; } = new Version(1, 0, 3);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
